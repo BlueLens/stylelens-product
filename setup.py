e13b2142b09dcb3038a73d086b3761e3a9013b27
Youngbok Yoon <master@bluehack.net>
2017-12-28 20:33:33 +0900

    version 1.0.40

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "stylelens-product"
-VERSION = "1.0.39"
+VERSION = "1.0.40"
 # To install the library, run the following
 #
 # python setup.py install
