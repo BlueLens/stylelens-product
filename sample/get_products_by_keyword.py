8b1c729468a598134a13510695a9472a93364e15
Daesub Kim <rano@bluehack.net>
2018-02-07 17:43:14 +0900

    modified get_products_by_keyword API

--- sample/get_products_by_keyword.py
+++ sample/get_products_by_keyword.py
@@ -5,7 +5,7 @@ api_instance = Products()
 
 try:
     # Added a new Product
-    api_response = api_instance.get_products_by_keyword('재킷', only_text=True)
+    api_response = api_instance.get_products_by_keyword('재킷', only_text=True, is_processed_for_text_class_model=False)
     pprint(api_response)
 except Exception as e:
     print("Exception when calling ProductApi->get_products_by_keyword: %s\n" % e)
