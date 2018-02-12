828af70170afd2a8dbaddf156777c1a7038fe5f2
Daesub Kim <rano@bluehack.net>
2018-02-12 16:33:49 +0900

    [STYL-205]: modified get_products_by_keyword sample code for checking 'coating' keyword

--- sample/get_products_by_keyword.py
+++ sample/get_products_by_keyword.py
@@ -4,8 +4,34 @@ from pprint import pprint
 api_instance = Products()
 
 try:
-    # Added a new Product
-    api_response = api_instance.get_products_by_keyword('t', only_text=True, offset=0, limit=100)
-    pprint(api_response)
+    # api_response = api_instance.get_products_by_keyword('Coat', only_text=True, is_processed_for_text_class_model=False, offset=0, limit=100)
+    # pprint(api_response)
+
+    keyword = 'coat'
+    test_str = 'coating'
+
+    offset = 0
+    limit = 100
+
+    while True:
+        api_response = api_instance.get_products_by_keyword(keyword, only_text=True,
+                                                            is_processed_for_text_class_model=False,
+                                                            offset=offset,
+                                                            limit=limit)
+
+        # pprint(api_response)
+
+        for res in api_response:
+            name = res.get('name')
+            if test_str in name:
+                pprint(test_str + ' in keyword: ' + keyword)
+                pprint(name)
+                pprint(res.get('cate'))
+                pprint(res.get('tags'))
+
+        if limit > len(api_response):
+            break
+        else:
+            offset = offset + limit
 except Exception as e:
     print("Exception when calling ProductApi->get_products_by_keyword: %s\n" % e)
